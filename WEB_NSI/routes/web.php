62ddf0dc497354cd891d5961e0d629851283b8a1
Abiyyuijlal29 <abiyyuijlalnovamatin29@gmail.com>
2026-04-27 08:00:28 +0700

    test

--- WEB_NSI/routes/web.php
+++ WEB_NSI/routes/web.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
 
-Route::inertia('/', 'welcome', [
-    'canRegister' => Features::enabled(Features::registration()),
-])->name('home');
+Route::get('/', function () {
+    return redirect()->route('login');
+});
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
